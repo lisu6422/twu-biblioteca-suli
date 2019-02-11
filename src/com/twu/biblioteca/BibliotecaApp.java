dd6d329a8fd06a474105d42a7f4a5887ed127905
sli <sli@thoughtworks.com>
2019-02-11 16:48:40 +0800

    [RELEASE1-4][suli] Add menu of options and execute menu selection.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,15 +3,34 @@ package com.twu.biblioteca;
 import com.twu.biblioteca.resource.BibliotecaResource;
 import com.twu.biblioteca.resource.Book;
 import java.util.List;
+import java.util.Scanner;
 
 public class BibliotecaApp {
+
+  private static final Integer OPTIONAL_1 = 1;
+  private static BibliotecaResource resource = BibliotecaResource.getInstance();
 
 
   public static void main(String[] args) {
     printWelcomeMessage();
 
-    BibliotecaResource resource = BibliotecaResource.getInstance();
-    printAllBookList(resource.getBookList());
+    printMenu();
+
+    Scanner scan = new Scanner(System.in);
+    executeMenuSelection(scan);
+  }
+
+  private static void executeMenuSelection(Scanner scan) {
+    if (scan.hasNextInt()) {
+      if (scan.nextInt() == OPTIONAL_1) {
+        printAllBookList(resource.getBookList());
+      }
+    }
+  }
+
+  private static void printMenu() {
+    System.out.println("*************menu*************");
+    System.out.println(String.format("[%s] List of books", OPTIONAL_1));
   }
 
   private static void printAllBookList(List<Book> bookList) {
